d8d20f0c0f9e70303c8038888eec0d285ed37f3f
Andrey Astashov <mvc.aaa@gmail.com>
2014-12-05 18:46:50 +0300

    Config files changes

--- config/web.php
+++ config/web.php
@@ -48,7 +48,11 @@ if (YII_ENV_DEV) {
     $config['modules']['debug'] = 'yii\debug\Module';
 
     $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '192.168.56.*'],
+    ];
+
 }
 
 return $config;
