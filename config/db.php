024a403aada61c00aa573a697638f8ec221ce3ce
Andrey Astashov <mvc.aaa@gmail.com>
2014-12-06 17:35:07 +0300

    Adding scripts

--- config/db.php
+++ config/db.php
@@ -1,11 +1,11 @@
 <?php
 
-if (array_key_exists('DB1_HOST' ,$_SERVER)) {
+if (array_key_exists('DB1_HOST' ,$_ENV)) {
     return [
         'class' => 'yii\db\Connection',
-        'dsn' => "mysql:host=" . $_SERVER['DB1_HOST'] . ";dbname=" . $_SERVER['DB1_NAME'],
-        'username' => $_SERVER['DB1_USER'],
-        'password' => $_SERVER['DB1_PASS'],
+        'dsn' => "mysql:host=" . $_ENV['DB1_HOST'] . ";dbname=" . $_ENV['DB1_NAME'],
+        'username' => $_ENV['DB1_USER'],
+        'password' => $_ENV['DB1_PASS'],
         'charset' => 'utf8',
     ];
 } else {
