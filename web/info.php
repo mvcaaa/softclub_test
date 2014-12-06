a1a905a3b08caf9f45801296f9aa640d3b4cfdf3
Andrey Astashov <mvc.aaa@gmail.com>
2014-12-06 15:57:41 +0300

    Adding scripts

---
+++ web/info.php
@@ -0,0 +1,9 @@
+<?PHP
+
+echo $_SERVER['DB1_HOST']."<BR>";
+echo $_SERVER['DB1_NAME']."<BR>";
+echo $_SERVER['DB1_PORT']."<BR>";
+echo $_SERVER['DB1_USER']."<BR>";
+echo $_SERVER['DB1_PASS']."<BR>";
+
+?>
