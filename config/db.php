99d0e84c167301dd532f5d7dcc4873391929e2c8
Andrey Astashov <mvc.aaa@gmail.com>
2014-12-06 16:11:22 +0300

    Adding scripts

--- config/db.php
+++ config/db.php
@@ -1,9 +1,19 @@
 <?php
 
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2',
-    'username' => 'yii2user',
-    'password' => '',
-    'charset' => 'utf8',
-];
+if ($_SERVER['DB1_HOST']) {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => "mysql:host=" . $_SERVER['DB1_HOST'] . ";dbname=" . $_SERVER['DB1_NAME'],
+        'username' => $_SERVER['DB1_USER'],
+        'password' => $_SERVER['DB1_PASS'],
+        'charset' => 'utf8',
+    ];
+} else {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yii2',
+        'username' => 'yii2user',
+        'password' => '',
+        'charset' => 'utf8',
+    ];
+}
