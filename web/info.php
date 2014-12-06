65444734f3b2f10533450181c7a583111040daa1
Andrey Astashov <mvc.aaa@gmail.com>
2014-12-06 17:56:59 +0300

    Adding scripts

--- web/info.php
+++ web/info.php
@@ -1,10 +1,10 @@
 <?PHP
 
-echo $_SERVER['DB1_HOST']."<BR>";
-echo $_SERVER['DB1_NAME']."<BR>";
-echo $_SERVER['DB1_PORT']."<BR>";
-echo $_SERVER['DB1_USER']."<BR>";
-echo $_SERVER['DB1_PASS']."<BR>";
+echo $_ENV['DB1_HOST']."<BR>";
+echo $_ENV['DB1_NAME']."<BR>";
+echo $_ENV['DB1_PORT']."<BR>";
+echo $_ENV['DB1_USER']."<BR>";
+echo $_ENV['DB1_PASS']."<BR>";
 
 var_dump ($_ENV);
 ?>
